250ffb29ce96ce296bd8e3d2f81900d1007cddac
rahmachehoud <rahma.kechich@gmail.com>
2024-05-13 13:41:26 +0100

    add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://rahmakechich:rahma123@cluster0.l8mc2tz.mongodb.net/'), UsersModule,],
   controllers: [AppController],
   providers: [AppService],
 })
